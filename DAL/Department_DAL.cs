b583bdf7556ad59c0a571a81342a3a38ccc9690b
Quý Hiệp Nguyễn <nquyhiep@gmail.com>
2022-01-14 21:48:19 +0700

    tạo function ReadDepartmentList và ReadDepartment

--- DAL/Department_DAL.cs
+++ DAL/Department_DAL.cs
@@ -10,5 +10,40 @@ namespace DAL
 {
     public class Department_DAL: DBConnection
     {
+        public List<Department_DTO> ReadDepartmentList()
+        {
+            SqlConnection conn = CreateConnection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select * from Department", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            List<Department_DTO> lstDepartment = new List<Department_DTO>();
+            while (reader.Read())
+            {
+                Department_DTO department = new Department_DTO();
+                department.IdDepartment = reader["IdDepartment"].ToString();
+                department.Name = reader["Name"].ToString();
+                lstDepartment.Add(department);
+            }
+            conn.Close();
+            return lstDepartment;
+        }
+        public Department_DTO ReadDepartment(string id)
+        {
+            SqlConnection conn = CreateConnection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select * from Department where IdDepartment=" + "'" + id.ToString() + "'", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            Department_DTO department = new Department_DTO();
+            if (reader.HasRows && reader.Read())
+            {
+                department.IdDepartment = reader["IdDepartment"].ToString();
+                department.Name = reader["Name"].ToString();
+
+            }
+            conn.Close();
+            return department;
+        }
     }
 }
