b6863e83901fa8076bfd5478325e6c5a20471188
Quý Hiệp Nguyễn <nquyhiep@gmail.com>
2022-01-14 21:49:18 +0700

    ReadEmployee

--- DAL/Employee_DAL.cs
+++ DAL/Employee_DAL.cs
@@ -12,5 +12,33 @@ namespace DAL
 {
     public class Employee_DAL:DBConnection
     {
+        public List<Employee_DTO> ReadEmployee()
+        {
+            SqlConnection conn = CreateConnection();
+            conn.Open();
+            //khỏi tạo instance của class SqlCommand
+            SqlCommand cmd = new SqlCommand();
+            //sử dụng thuộc tính CommandText để chỉ định tên Proc
+            cmd.CommandText = "spReadEmployee";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = conn;
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Employee_DTO> lstNv = new List<Employee_DTO>();
+            Department_DAL department = new Department_DAL();
+
+            while (reader.Read())
+            {
+                Employee_DTO nv = new Employee_DTO();
+                nv.IdEmployee = reader["IdEmployee"].ToString();
+                nv.Name = reader["Name"].ToString();
+                nv.DateBirth = DateTime.Parse(reader["DateBirth"].ToString());
+                nv.Gender = bool.Parse(reader["Gender"].ToString());
+                nv.PlaceBirth = reader["PlaceBirth"].ToString();
+                nv.Department = department.ReadDepartment(reader["IdDepartment"].ToString());
+                lstNv.Add(nv);
+            }
+            conn.Close();
+            return lstNv;
+        }
     }
 }
