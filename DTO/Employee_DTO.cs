faf2a0433c69b67a6c7fd26a86f94910e1f22fc9
Quý Hiệp Nguyễn <nquyhiep@gmail.com>
2022-01-14 21:29:39 +0700

    tạo các class DTO

---
+++ DTO/Employee_DTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Employee_DTO
+    {
+        public string IdEmployee { get; set; }
+        public string Name { get; set; }
+        public DateTime DateBirth { get; set; }
+        public bool Gender { get; set; }
+        public string PlaceBirth { get; set; }
+
+        public Department_DTO Department { get; set; }
+        public string DepamentName
+        {
+
+            get { return Department.Name; }
+
+        }
+    }
+}
